c41fc1707bac85402fb5e74bf9368cb071553339
МеушР <@N215-3>
2020-02-27 21:01:36 +0600

    asd

---
+++ Soap_Cons/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soap_Cons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WS.WSSoapClient wSSoapClient = new WS.WSSoapClient();
+
+            var res = wSSoapClient.First(3);
+
+            foreach (var item in wSSoapClient.Second())
+            {
+                Console.WriteLine($"{item.BookName}, {item.Publish}, {item.bookLocation.LocationName1}, {item.bookLocation.LocationName2} ");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ WebApiPoj/WS.asmx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using WebApiPoj.Models;
+
+namespace WebApiPoj
+{
+    /// <summary>
+    /// Сводное описание для WS
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Чтобы разрешить вызывать веб-службу из скрипта с помощью ASP.NET AJAX, раскомментируйте следующую строку. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WS : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public book First(int id)
+        {          
+            return Repo.DataBooks.Where(z => z.Id == id).FirstOrDefault();
+        }
+
+        [WebMethod]
+        public List<book> Second()
+        {
+            return Repo.DataBooks;
+        }
+
+        [WebMethod]
+        public List<book> getBooks(int startYear, int lastYear)
+        {
+            var b = Repo.DataBooks.Where(w => Int32.Parse(w.Year) >= startYear && Int32.Parse(w.Year) <= lastYear).ToList();
+            return b;
+        }
+
+        [WebMethod]
+        public int getCountBooks()
+        {          
+            return Repo.DataBooks.Count;
+        }
+    }
+}
